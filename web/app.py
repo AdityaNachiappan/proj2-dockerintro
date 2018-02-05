d1d1454e5e9ca5aed662029f778ee9ed051cc0dd
AdityaNachiappan <35346621+AdityaNachiappan@users.noreply.github.com>
2018-02-05 13:19:34 -0800

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,57 +1,31 @@
 import os
 from flask import Flask
-from flask import render_template
-from flask import make_response
 
 app = Flask(__name__)
-
-# Get parent directory's path
-this_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-class FileNotFound(Exception):
-    pass
-
-@app.errorhandler(FileNotFound)
-def file_not_found_error_handler(error):
-
-    return make_response(render_template("404.html"),
-        404 
-    )
-
-
-class FileForbidden(Exception):
-    pass
-
-@app.errorhandler(FileForbidden)
-def file_forbidden_error_handler(error):
-    return make_response(
-        render_template("403.html"),
-        403
-    )
-
 
 @app.route("/")
 def hello():
     return "UOCIS docker demo!"
 
 
-@app.route(methods=['GET', 'Post'])
-def fetch_html(file_name):
+@app.route("/name")
+def hello():
+    if os.path.isfile("./templates/" + name):
+		return render_template(name), 200
+    elif "//" in name or ".." in name or "~" in name :
+        abort(403)
+    elif  ".html" not in name and ".css" not in name:
+        abort(404)
+  
 
-    file_path = os.path.join(this_dir, "templates", file_name)
+@app.errorhandler(404)
+def page_not_found(error):
+	return flask.render_template('404.html'),404
 
-    if os.path.isfile(file_path) is True:
-
-        return make_response(
-            render_template(file_name),
-            200  # OK
-        )
-    else:
-        raise FileNotFound
+@app.errorhandler(403)
+def page_not_found(error):
+	return flask.render_template('403.html'),403	
 
 
-
-# Start the server
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True,host='0.0.0.0')
